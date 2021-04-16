f0d982a26256b11619a191603941bf45042bd01f
fc1943s <fc1943s@users.noreply.github.com>
2021-04-16 21:09:04 +0000

    deploy: c7157bfb248e2ca59233775c157982a96468f911

---
+++ precache-manifest.15f98fcb53997f3fb31a022fdcd5a963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a4547efc36d4ae9050e7dd65117368",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0470a9583d3521696a7f",
+    "url": "/static/css/main.50841ac9.chunk.css"
+  },
+  {
+    "revision": "692bdc973b7c8e3293fb",
+    "url": "/static/js/2.00befc03.chunk.js"
+  },
+  {
+    "revision": "2bb19d501c0f26f7d6e414162f06d909",
+    "url": "/static/js/2.00befc03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0470a9583d3521696a7f",
+    "url": "/static/js/main.2e4fee7c.chunk.js"
+  },
+  {
+    "revision": "c7714846211550bb5c7b",
+    "url": "/static/js/runtime-main.14689278.js"
+  }
+]);
